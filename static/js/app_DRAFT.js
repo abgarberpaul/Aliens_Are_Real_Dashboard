501b81ef0b8cd6adf8ad178caa0470499027919b
Aaron Garber-Paul <aaron@MacBook-Pro-2.local>
2021-01-09 20:51:33 -0800

    in progress

--- static/js/app_DRAFT.js
+++ static/js/app_DRAFT.js
@@ -27,7 +27,7 @@ function selectdate(){
 var datetime = d3.select("#datetime").property("value")
 var filterdata = data.filter((sighting) => {
   return sighting.datetime == datetime;
-});
+})
 
 // BUTTON: Country
 var country = d3.select("#country").property("value")
@@ -36,37 +36,63 @@ var filterdata = filterdata.filter((sighting) => {
   return sighting.country == country;
 })
 
-// // // BUTTON: State
-// var state = d3.select("#state").property("value")
-// if(state){
-// var filterdata = filterdata.filter((sighting) => {
-//   return sighting.state == state;
-// })
+// // BUTTON: State
+var state = d3.select("#state").property("value")
+if(state){
+var filterdata = filterdata.filter((sighting) => {
+  return sighting.state == state;
+})
 
-// // // BUTTON: City
-// var city = d3.select("#city").property("value")
-// if(city){
-// var filterdata = filterdata.filter((sighting) => {
-//   return sighting.city == city;
-// })
+// // BUTTON: City
+var city = d3.select("#city").property("value")
+if(city){
+var filterdata = filterdata.filter((sighting) => {
+  return sighting.city == city;
+})
 
-// // BUTTON: Shape
-// var shape = d3.select("#shape").property("value")
-// if(shape){
+// BUTTON: Shape
+var shape = d3.select("#shape").property("value")
+if(shape){
+var filterdata = filterdata.filter((sighting) => {
+  return sighting.shape == shape;
+})
+}
+}
+}
+}
+// // BUTTON: VAR DURATION
+// var duration = d3.select("#duration").property("value")
+// if(duration){
 // var filterdata = filterdata.filter((sighting) => {
-//   return sighting.shape == shape;
+//   return sighting.durationMinutes == duration;
 // })
 // }
-// }
-// }
-// }
-// // // BUTTON: VAR DURATION
-// // var duration = d3.select("#duration").property("value")
-// // if(duration){
-// // var filterdata = filterdata.filter((sighting) => {
-// //   return sighting.durationMinutes == duration;
-// // })
-// // }
+
+//set table to blank before search
+tbody.html("")
+
+//Rebuilds table based on search filter:
+filterdata.forEach(function(UFO_Data) {
+    console.log(UFO_Data);
+    var row = tbody.append("tr");
+    Object.entries(UFO_Data).forEach(function([key, value]) {
+      console.log(key, value);
+      // Append a cell to the row for each value
+      // in the weather report object
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+}
+
+// // BUTTON: WORD IN TEXT
+
+function descriptionSearch(){
+var descriptionSearch = d3.select("#descriptionSearch").property("value")
+if(descriptionSearch){
+var filterdata = filterdata.filter((sighting) => {
+  return sighting.comments == descriptionSearch;
+})
 
 //set table to blank before search
 tbody.html("")
@@ -85,6 +111,7 @@ filterdata.forEach(function(UFO_Data) {
   });
 }
 }
+
 //sets the page up to "listen" to perform function on click event
 d3.select("#filter-btn").on("click", selectdate)
 
